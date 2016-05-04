314367cbcbde8eb7e68132ad0b2fa3aa3fefc482
wangsl <wangsl2001@gmail.com>
2016-05-03 23:49:02 -0400

    modified argument for GaussLegendre2

--- GaussLegendre/GaussLegendre2.m
+++ GaussLegendre/GaussLegendre2.m
@@ -1,10 +1,13 @@
-function [ x, w ] = GaussLegendre2(m)
 
-assert( 1 <= m && m <= 599)
+function [ x, w ] = GaussLegendre2(n)
+
+if n < 1 || n > 599 
+  error('argument should be 1 <= n <= 599')
+end
 
 data_dir = '/home/wang/matlab/quantum-dynamics/GaussLegendre';
 
-grids_file = strcat(data_dir, '/', int2str(m), '-LegendreGauss.grids');
+grids_file = strcat(data_dir, '/', int2str(n), '-LegendreGauss.grids');
 
 if exist(grids_file, 'file') ~= 2 
   error('"%s" does not exit\n', grids_file)
@@ -13,9 +16,9 @@ end
 [ x, w ] = textread(grids_file, '%f %f');
 
 assert(isequal(size(x), size(w)))
-assert(m == 2*numel(x) - mod(m,2))
+assert(n == 2*numel(x) - mod(n,2))
 
-if mod(m, 2) 
+if mod(n, 2) 
   x = cat(1, -x(end:-1:2), x);
   w = cat(1,  w(end:-1:2), w);
 else
